6a22862ebeb35a0524598fcea27cbe0821a32f8b
alisonHernandez <alisonhm.ti22@utsjr.edu.mx>
2026-04-26 13:12:28 -0600

    remove hardcoded firebase keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQCjJES-F0aS7cb6T2U_iVglUThVEe1lU",
-  authDomain: "integramedica-5f5da.firebaseapp.com",
-  projectId: "integramedica-5f5da",
-  storageBucket: "integramedica-5f5da.firebasestorage.app",
-  messagingSenderId: "774658168996",
-  appId: "1:774658168996:web:0a6ef22802838664396e31",
-  measurementId: "G-KWT29MPHD3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp;
